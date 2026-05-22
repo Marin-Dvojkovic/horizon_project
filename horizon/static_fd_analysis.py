8c55e1978585368bb3fa5c3290cc42ba746d4275
Sara Lhamo Schnaterbeck <159925286+saralhamo@users.noreply.github.com>
2026-05-22 16:49:44 +0200

    Matplotlib for igraph (#10)
    * switch to matplotlib
    
    * added enable_plotting to FDGraph

--- horizon/static_fd_analysis.py
+++ horizon/static_fd_analysis.py
@@ -1,68 +1,20 @@
 import sys
 from pathlib import Path
-from symtable import Function
-from textwrap import indent
 
 import igraph as ig
+import matplotlib.pyplot as plt
 from igraph import Graph
 from utils.fd import FunctionalDependency
 
 output_dir: Path = Path("output")
+enable_plotting: bool = True
+
+# Use matplotlib as backend for igraph
+ig.config["plotting.backend"] = "matplotlib"
 
 # Global variables for storing FDs
 set_of_fds: list[FunctionalDependency] = []
 bound_attributes: set[str] = set()
-
-
-def determine_boundedness(fds: list[FunctionalDependency]) -> None:
-    global bound_attributes
-
-    attribute_fd_mapping: dict[str, list[tuple[str, str]]] = {}
-    free_attributes: set[str] = set()
-    fds_to_check: list[tuple[str, str]] = []
-
-    # TODO: Deal with multiple attributes on LHS correctly
-    # Iterate over all FDs
-    for fd in fds:
-        rhs: str = fd.rhs
-        if rhs not in attribute_fd_mapping:
-            attribute_fd_mapping[rhs] = []
-
-        # Always add RHS attributes to free attributes
-        free_attributes.add(rhs)
-        if rhs in bound_attributes:
-            bound_attributes.remove(rhs)
-
-        # For all LHS attributes
-        for lhs in fd.lhs:
-            # Collect all FDs that point to the respective attribute
-            if lhs not in attribute_fd_mapping:
-                attribute_fd_mapping[lhs] = []
-            attribute_fd_mapping[rhs].append((lhs, rhs))
-
-            # If LHS not in free attributes: Add LHS attribute to bound attributes
-            if lhs not in free_attributes:
-                bound_attributes.add(lhs)
-                continue
-
-            # If LHS in free attributes:
-            # If there is no cycle LHS <-> RHS, LHS stays free
-            if (rhs, lhs) not in attribute_fd_mapping[lhs]:
-                continue
-            # If there is a cycle LHS <-> RHS, but another attribute A bounds one of the attributes (A -> LHS or A -> RHS), LHS stays free
-            if len(attribute_fd_mapping[lhs]) > 1 or len(attribute_fd_mapping[rhs]) > 1:
-                continue
-            # Else we don't know yet and have to check at the end
-            fds_to_check.append((lhs, rhs))
-
-    # Check unprocessed cycles LHS <-> RHS, if A with A -> LHS or A -> RHS appeared, both LHS and RHS stay free
-    for lhs, rhs in fds_to_check:
-        # Add LHS to bound attributes, if there is no other attribute A
-        if len(attribute_fd_mapping[lhs]) == 1 and len(attribute_fd_mapping[rhs]) == 1:
-            free_attributes.remove(lhs)
-            bound_attributes.add(lhs)
-
-    print(f"Found {len(bound_attributes)} bound attribute(s): {bound_attributes}.")
 
 
 # Build FD graph
@@ -78,13 +30,21 @@ def build_fd_graph() -> ig.Graph:
         edge_attrs="fd_index",
     )  # TODO: Support hyperedges
 
-    ig.plot(
-        g,
-        vertex_color="green",
-        vertex_label=g.vs["name"],
-        edge_label=g.es["fd_index"],
-        target=output_dir / "fd_graph.png",
-    )
+    if enable_plotting:
+        plt.figure(figsize=(max(7, g.vcount() / 2), max(7, g.vcount() / 2)))
+
+        ig.plot(
+            g,
+            layout="tree",
+            vertex_color="green",
+            vertex_label=g.vs["name"],
+            vertex_label_dist=1.5,
+            edge_label=g.es["fd_index"],
+        )
+
+        plt.gca().invert_yaxis()  # Plot tree with root at top
+        plt.savefig(str(output_dir / "fd_graph.png"))
+        plt.clf()
 
     print(f"FD graph has {g.vcount()} vertices and {g.ecount()} edges.")
 
@@ -95,12 +55,21 @@ def build_fd_graph() -> ig.Graph:
 def find_strongly_connected_components(g: ig.Graph) -> ig.Graph:
     # Compute and visualize components
     components: ig.VertexClustering = ig.Graph.components(g)
-    ig.plot(
-        components,
-        palette=ig.RainbowPalette(),
-        vertex_label=g.vs["name"],
-        target=output_dir / "fd_graph_components.png",
-    )
+
+    if enable_plotting:
+        plt.figure(figsize=(max(7, g.vcount() / 2), max(7, g.vcount() / 2)))
+
+        ig.plot(
+            components,
+            layout="tree",
+            palette=ig.RainbowPalette(),
+            vertex_label=g.vs["name"],
+            vertex_label_dist=1.5,
+        )
+
+        plt.gca().invert_yaxis()  # Plot tree with root at top
+        plt.savefig(str(output_dir / "fd_graph_components.png"))
+        plt.clf()
 
     # Build and visualize cluster graph (SCCG)
     scc_g: ig.Graph = components.cluster_graph(
@@ -109,13 +78,22 @@ def find_strongly_connected_components(g: ig.Graph) -> ig.Graph:
         },
         combine_edges={"fd_index": lambda fd_indices: fd_indices},
     )
-    ig.plot(
-        scc_g,
-        vertex_color="green",
-        vertex_label=scc_g.vs["name"],
-        edge_label=scc_g.es["fd_index"],
-        target=output_dir / "scc_fd_graph.png",
-    )
+
+    if enable_plotting:
+        plt.figure(figsize=(max(7, scc_g.vcount() / 2), max(7, scc_g.vcount() / 2)))
+
+        ig.plot(
+            scc_g,
+            layout="tree",
+            vertex_color="green",
+            vertex_label=scc_g.vs["name"],
+            vertex_label_dist=1.5,
+            edge_label=scc_g.es["fd_index"],
+        )
+
+        plt.gca().invert_yaxis()  # Plot tree with root at top
+        plt.savefig(str(output_dir / "scc_fd_graph.png"))
+        plt.clf()
 
     print(f"SCC graph has {scc_g.vcount()} components and {scc_g.ecount()} edges.")
 
@@ -211,7 +189,7 @@ def order_subg(sub_g: ig.Graph) -> list[FunctionalDependency]:
 
 # Implements Kahn's Algorithm for computing a topological sorting using BFS
 def get_topological_sorting(
-    component_g: ig.Graph, originalg: ig.Graph
+    component_g: ig.Graph, original_g: ig.Graph
 ) -> list[FunctionalDependency]:
     ordered_fds: list[FunctionalDependency] = []
 
@@ -224,8 +202,8 @@ def get_topological_sorting(
         # If component contains more than 1 vertex
         if len(component_v["name"]) > 1:
             # Compute order of this component via Eulerian tour of subgraph
-            sub_g = originalg.induced_subgraph(
-                originalg.vs.select(name_in=component_v["name"]),
+            sub_g = original_g.induced_subgraph(
+                original_g.vs.select(name_in=component_v["name"]),
                 implementation="create_from_scratch",
             )
             ordered_fds += order_subg(sub_g)
@@ -246,7 +224,7 @@ def get_topological_sorting(
 
 
 def order_fds(g: ig.Graph, scc_g: ig.Graph) -> list[list[FunctionalDependency]]:
-    # Perform topological sorting of each subgraph of SCCG
+    # Perform topological sorting of each sub-component of SCCG
     ordered_fds: list[list[FunctionalDependency]] = [
         get_topological_sorting(sub_g, g) for sub_g in scc_g.decompose(mode="weak")
     ]
@@ -269,9 +247,6 @@ def get_ordered_fds(
     # Create output directory
     if not output_dir.exists:
         output_dir.mkdir()
-
-    # Determine attribute boundedness
-    determine_boundedness(set_of_fds)
 
     # Build FD graph
     g: ig.Graph = build_fd_graph()
