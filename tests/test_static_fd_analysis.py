af227d6a8cb9c7fcfd8f51910e76f5a43a2722f7
Sara Lhamo Schnaterbeck <159925286+saralhamo@users.noreply.github.com>
2026-07-21 23:54:48 +0200

    added new test cases

--- tests/test_static_fd_analysis.py
+++ tests/test_static_fd_analysis.py
@@ -18,7 +18,55 @@ set_of_fds: dict[str, SetOfFDs] = {
             FunctionalDependency("provider_id", "provider_address"),
             FunctionalDependency("service_area", "provider_area_id"),
         ]
-    )
+    ),
+    "interaction_case_1": SetOfFDs(
+        [
+            FunctionalDependency("A", "B"),
+            FunctionalDependency("A", "C"),
+            FunctionalDependency("A", "D"),
+        ]
+    ),
+    "interaction_case_2": SetOfFDs(
+        [
+            FunctionalDependency("A", "C"),
+            FunctionalDependency("B", "C"),
+            FunctionalDependency("D", "C"),
+        ]
+    ),
+    "interaction_case_3": SetOfFDs(
+        [
+            FunctionalDependency("A", "B"),
+            FunctionalDependency("B", "C"),
+            FunctionalDependency("C", "D"),
+        ]
+    ),
+    "interaction_case_4": SetOfFDs(
+        [
+            FunctionalDependency("A", "B"),
+            FunctionalDependency("B", "A"),
+        ]
+    ),
+    "interaction_case_4a": SetOfFDs(
+        [
+            FunctionalDependency("A", "B"),
+            FunctionalDependency("B", "C"),
+            FunctionalDependency("C", "B"),
+        ]
+    ),
+    "interaction_case_4b": SetOfFDs(
+        [
+            FunctionalDependency("A", "B"),
+            FunctionalDependency("B", "A"),
+            FunctionalDependency("B", "C"),
+        ]
+    ),
+    "hyperedges": SetOfFDs(
+        [
+            FunctionalDependency("A", "C"),
+            FunctionalDependency("B", "D"),
+            FunctionalDependency(("A", "B"), "E"),
+        ]
+    ),
 }
 
 correct_ordered_fds: dict[str, list[FunctionalDependency]] = {
@@ -27,15 +75,67 @@ correct_ordered_fds: dict[str, list[FunctionalDependency]] = {
         FunctionalDependency("provider_address", "provider_area_id"),
         FunctionalDependency("provider_area_id", "service_area"),
         FunctionalDependency("service_area", "provider_area_id"),
-    ]
+    ],
+    "interaction_case_1": [
+        FunctionalDependency("A", "D"),
+        FunctionalDependency("A", "C"),
+        FunctionalDependency("A", "B"),
+    ],
+    "interaction_case_2": [
+        FunctionalDependency("D", "C"),
+        FunctionalDependency("B", "C"),
+        FunctionalDependency("A", "C"),
+    ],
+    "interaction_case_3": [
+        FunctionalDependency("A", "B"),
+        FunctionalDependency("B", "C"),
+        FunctionalDependency("C", "D"),
+    ],
+    "interaction_case_4": [
+        FunctionalDependency("A", "B"),
+        FunctionalDependency("B", "A"),
+    ],
+    "interaction_case_4a": [
+        FunctionalDependency("A", "B"),
+        FunctionalDependency("B", "C"),
+        FunctionalDependency("C", "B"),
+    ],
+    "interaction_case_4b": [
+        FunctionalDependency("A", "B"),
+        FunctionalDependency("B", "A"),
+        FunctionalDependency("B", "C"),
+    ],
+    "hyperedges": [
+        FunctionalDependency("B", "D"),
+        FunctionalDependency("A", "C"),
+        FunctionalDependency(("A", "B"), "E"),
+    ],
 }
 
-correct_bound_attributes: dict[str, set[str]] = {"paper_example": {"provider_id"}}
+correct_bound_attributes: dict[str, set[str]] = {
+    "paper_example": {"provider_id"},
+    "interaction_case_1": {"A"},
+    "interaction_case_2": {"A", "B", "D"},
+    "interaction_case_3": {"A"},
+    "interaction_case_4": {"A"},
+    "interaction_case_4a": {"A"},
+    "interaction_case_4b": {"A"},
+    "hyperedges": {"A", "B"},
+}
 
 
 @pytest.fixture
 def all_test_datasets() -> list[str]:
-    return ["paper_example"]
+    return [
+        "paper_example",
+        "interaction_case_1",
+        "interaction_case_2",
+        "interaction_case_3",
+        "interaction_case_4",
+        "interaction_case_4a",
+        "interaction_case_4b",
+        "hyperedges",
+    ]
 
 
 def static_fd_analysis_test(dataset_name: str) -> None:
